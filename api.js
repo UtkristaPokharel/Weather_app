3e932bd1393cb2d250ff89f61bfd4e11e1bb0067
UtkristaPokharel <pokharelutkrista@gmail.com>
2025-01-23 23:48:14 +0545

    updated api

--- api.js
+++ api.js
@@ -1,9 +1,19 @@
 const apiKey="VMRT9NRKNJZLL3X5N3ERHPM2N";
-const apiUrl="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/butwal?unitGroup=us&contentType=json";
+const apiUrl="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Butwal?unitGroup=us&contentType=json";
 
 export async function checkWeather() {
 	const response =await fetch(apiUrl +`&key=${apiKey}`);
 	var data =await response.json();
 
 	console.log (data);
+
+	document.querySelector(".city").innerHTML=data.address;
+	document.querySelector(".temp").innerHTML=data.currentConditions.feelslike;
+	;
+	document.querySelector(".condition").innerHTML=data.currentConditions.conditions;
+	document.querySelector("#sunrise").innerHTML=data.currentConditions.sunrise;
+	document.querySelector("#sunset").innerHTML=data.currentConditions.sunset;
+	document.querySelector("#humidity").innerHTML=data.currentConditions.humidity;
+	document.querySelector("#rainfall").innerHTML=data.currentConditions.precipprob;
+	document.querySelector("#wind").innerHTML=data.currentConditions.windspeed;
 }
