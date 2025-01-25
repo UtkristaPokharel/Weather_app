8f09dd94c44d6bc04f344249d703d55ae345dfe5
UtkristaPokharel <pokharelutkrista@gmail.com>
2025-01-25 10:21:52 +0545

    Writing comments and finalizing

--- api.js
+++ api.js
@@ -1,12 +1,15 @@
+//Api Key
 const apiKey = "VMRT9NRKNJZLL3X5N3ERHPM2N";
 
 const searchBox = document.querySelector(".search-bar input");
 const searchBtn = document.querySelector(".search-bar button");
 const weatherIcon = document.querySelector(".weather_condition");
 
+//getting time for day and night
 const currentHour = new Date().getHours();
 const isDay = currentHour >= 6 && currentHour < 18;
 
+//search button when clicked
 searchBtn.addEventListener("click", () => {
 	const city = searchBox.value.trim();
 	if (city) {
@@ -16,6 +19,7 @@ searchBtn.addEventListener("click", () => {
 	}
 });
 
+//If enter button is clicked 
 searchBox.addEventListener("keydown", (event) => {
 	if (event.key === "Enter") {
 		const city = searchBox.value.trim();
@@ -27,9 +31,11 @@ searchBox.addEventListener("keydown", (event) => {
 	}
 });
 
+//Asynchronous Function for fetching weather data from api
 export async function checkWeather(city) {
 	const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&contentType=json`;
 
+	//for any errors arising
 	try {
 		const response = await fetch(apiUrl + `&key=${apiKey}`);
 		if (!response.ok) {
@@ -38,9 +44,7 @@ export async function checkWeather(city) {
 
 		const data = await response.json();
 
-		console.log(data); // Log API data for debugging
-
-		// Update UI with weather data
+		// Update weather data in their respective icons
 		document.querySelector(".city").innerHTML = data.address;
 		document.querySelector(".temp").innerHTML = data.currentConditions.temp + "°C";
 		document.querySelector(".condition").innerHTML = data.currentConditions.conditions;
@@ -51,6 +55,7 @@ export async function checkWeather(city) {
 		document.querySelector("#wind").innerHTML = data.currentConditions.windspeed + " km/h";
 
 
+		//condition for sunny, rainy, night 
 		const condition = data.currentConditions.conditions.toLowerCase();
 		if (condition === "clear") {
 			weatherIcon.src = isDay ? "img/sunny.png" : "img/clear_night.png";
@@ -61,7 +66,8 @@ export async function checkWeather(city) {
 		} else {
 			weatherIcon.src = isDay ? "img/sunny.png" : "img/clear_night.png";
 		}
-	} catch (error) {
+	} //Catching error
+	catch (error) {
 		console.error("Error fetching weather data:", error);
 		alert("Failed to fetch weather data. Please check the city name and try again.");
 	}
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//importing Api.js
+//wanted to do some DOM work but lazy
 import { checkWeather } from "./api.js";
 
 checkWeather();
